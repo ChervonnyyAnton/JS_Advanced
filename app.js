214bfdb25cdad746a5c0b14b3e9f767f5965d765
ChervonnyyAnton <chervonnyy@icloud.com>
2023-05-10 09:46:02 +0200

    cart management

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
 'use strict'
 
-function timer(sec){
-	let countdown = sec * 1000;
-	console.log(`${sec} seconds timer start!`);
-	const interval = setInterval(() => {
-		countdown -= 1000;
-		console.log(`${countdown / 1000} seconds left`);
-	}, 999);
-	setTimeout(() => {
-		clearInterval(interval);
-		console.log("Your pizza is ready!");
-		console.log(`${sec} seconds timer is over!`);
-  }, countdown);
+let count = 0;
+let index = 0;
+
+const Product = function(name, amount){
+	count++;
+	this.id = count;
+	this.name = name;
+	this.amount = amount;
 }
 
-timer(15);
+const Basket = function(){
+	index++;
+	this.id = index;
+	this.products = [];
+}
+
+Basket.prototype.addProduct = function(product){
+	if(this.products.find(p => p.id === product.id)){
+		p.increaseBy(1);
+	}
+
+	this.products.push(product);
+}
+
+Basket.prototype.decrease = function (id) {
+  this.products = this.products
+  	.map((p) => {
+    	if (p.id === id) {
+      		p.amount--;
+    	}
+    	return p;
+  	})
+	.filter(p => p.amount > 0);
+};
+
+Basket.prototype.increase = function (id) {
+  this.products = this.products.map(p => {
+	if(p.id === id) {
+		p.amount++;
+	}
+	return p;
+  })
+};
+
+const bread = new Product('Bread', 1);
+const newBasket = new Basket();
+
+newBasket.addProduct(bread);
+newBasket.increase(1);
+newBasket.decrease(1);
+newBasket.decrease(1);
+
+console.log(newBasket);
