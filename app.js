c13bec2bcf4ffbb7b51a400ae3a6b2a26fb907e3
ChervonnyyAnton <chervonnyy@icloud.com>
2023-06-12 08:59:02 +0200

    average price

--- app.js
+++ app.js
@@ -1,60 +1,16 @@
 'use strict'
 
 /*
-	Сделать класс врага со здоровьем и методом получения урона.
-	Сделать клаcc меча, который имеет силу и метод нанесения урона.
-	Сделать класс орка, который в 50% случаев не получает урон.
+	Получить среднюю цену 30 товаров из API
 */
 
-class Enemy {
-	health;
-	constructor(health){
-		this.health = health;
-	}
+const request = new XMLHttpRequest();
+request.open('GET', 'https://dummyjson.com/products');
+request.send();
 
-	receiveDamage(damage){
-		this.health = this.health - damage;
-		console.log(this.health);
-	}
-}
-
-class Weapon {
-	#damage;
-	constructor(damage){
-		this.#damage = damage;
-	}
-
-	dealDamageTo(enemy) {
-		enemy.receiveDamage(this.#damage);
-	}
-}
-
-class Orc extends Enemy {
-	constructor(health){
-		super(health);
-	}
-
-	receiveDamage(damage) {
-		if(Math.random() > 0.5){
-			this.health = this.health - damage;
-		}
-		console.log(this.health);
-	}
-}
-
-const AngryOrk = new Orc(100);
-const Sword = new Weapon(10);
-
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
-Sword.dealDamageTo(AngryOrk);
+request.addEventListener('load', function() {
+	const { products } = JSON.parse(this.responseText);
+	console.log(products);
+	const sum = products.reduce((acc, prod) => acc += prod.price, 0);
+	console.log(sum / products.length);
+});
