1a78de1cc4d9b114adbacc5dd8a1857a262d2c37
ChervonnyyAnton <chervonnyy@icloud.com>
2023-08-01 09:03:47 +0200

    create select options

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 "use strict";
 
+/*
 fetch("https://dummyjson.com/products")
-  .then(
-    (response) => response.json(),
-  )
-  .then(
-    ({ products }) => {
-      console.log(products);
-      return fetch(`https://dummyjson.com/products/${products[0].id}`);
-    },
-  )
-  .then(
-    (response) => response.json(),
-  )
+  .then((response) => response.json())
+  .then(({ products }) => {
+    console.log(products);
+    return fetch(`https://dummyjson.com/products/${products[0].id}`);
+  })
+  .then((response) => response.json())
   .then((data) => {
     console.log(data);
   })
-  .catch(error => console.log(error))
+  .catch((error) => console.log(error))
   .finally(() => console.log("completed"));
+*/
+
+function createSelect(array) {
+  const element = document.querySelector(".filter");
+  element.innerHTML = `<select>
+		${array.map((el) => `<option value= ${el}>${el}</option>`)}
+	</select>`;
+}
+
+function getCategories() {
+  fetch("https://dummyjson.com/products/categories")
+    .then((response) => response.json())
+    .then((data) => createSelect(data))
+    .catch((error) => console.log(error))
+    .finally(() => console.log("complete"));
+}
+
+getCategories();
