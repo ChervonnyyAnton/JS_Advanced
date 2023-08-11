4f979092bb0601c72b913a93308e186845342423
ChervonnyyAnton <chervonnyy@icloud.com>
2023-08-11 09:34:27 +0200

    all promises

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
 "use strict";
 
-/*
-function getMyCoordinates() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      ({ coords }) => {
-        resolve({
-          latitude: coords.latitude,
-          longitude: coords.longitude,
-        });
-      },
-      (error) => {
-        reject(error);
-      }
-    );
-  });
-}
-*/
+const goodUrl = 'https://dummyjson.com/products/';
+const badUrl = 'https://dummyjson.com/productss/';
 
-async function getMyCity() {
-  try {
-    //const { latitude, longitude } = await getMyCoordinates();
-	const response = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=48.210033&longitude=16.363449`
-  );
-	if(!response){
-		throw new Error(response.status);
-	}
-	const data = await response.json();
-	console.log(data.city);
-  } catch (e) {
-    console.error(e);
+
+async function getProducts(id, url) {
+  let response;
+
+  if(id === null || id === undefined)
+  {
+    response = await fetch(url);
   }
+  else
+  {
+    response = await fetch(url + id);
+  }
+
+  return response.json();
 }
 
-getMyCity();
+async function main() {
+  const result_1 = await Promise.all([
+    getProducts(1, goodUrl),
+    getProducts(2, goodUrl),
+    getProducts(3, goodUrl)
+  ]);
+
+  console.log(result_1);
+
+  const result_2 = await Promise.allSettled([
+    getProducts(4, goodUrl),
+    getProducts(5, badUrl),
+    getProducts(6, goodUrl)
+  ]);
+
+    console.log(result_2);
+
+    const result_3 = await Promise.race([
+    getProducts(7, goodUrl),
+    getProducts(8, goodUrl),
+    getProducts(9, goodUrl)
+  ]);
+
+    console.log(result_3);
+}
+
+main();
