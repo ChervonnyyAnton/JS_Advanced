fb64037286694a60431419fd5f0b28cd12b44458
ChervonnyyAnton <chervonnyy@icloud.com>
2023-08-17 08:50:12 +0200

    post request

--- app.js
+++ app.js
@@ -1,48 +1,31 @@
 "use strict";
 
-const goodUrl = 'https://dummyjson.com/products/';
-const badUrl = 'https://dummyjson.com/productss/';
+const goodUrl = "https://boredapi.com/api/activity";
+const badUrl = "https://dummyjson.com/productss/";
 
+const credentials = {
+  username: "kminchelle",
+  password: "0lelplR",
+};
 
-async function getProducts(id, url) {
-  let response;
+const headers = {
+  "Content-Type": "application/json",
+};
 
-  if(id === null || id === undefined)
-  {
-    response = await fetch(url);
-  }
-  else
-  {
-    response = await fetch(url + id);
-  }
+const method = "POST";
 
-  return response.json();
+function request(method, headers, body) {
+  return {
+    method: method,
+    headers: headers,
+    body: JSON.stringify(body),
+  };
 }
 
 async function main() {
-  const result_1 = await Promise.all([
-    getProducts(1, goodUrl),
-    getProducts(2, goodUrl),
-    getProducts(3, goodUrl)
-  ]);
-
-  console.log(result_1);
-
-  const result_2 = await Promise.allSettled([
-    getProducts(4, goodUrl),
-    getProducts(5, badUrl),
-    getProducts(6, goodUrl)
-  ]);
-
-    console.log(result_2);
-
-    const result_3 = await Promise.race([
-    getProducts(7, goodUrl),
-    getProducts(8, goodUrl),
-    getProducts(9, goodUrl)
-  ]);
-
-    console.log(result_3);
+  const response = await fetch(goodUrl, request(method, headers, credentials));
+  const data = await response.json();
+  console.log(data);
 }
 
 main();
